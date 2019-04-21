4d3d93ba22a44119dfa51ece1c580839165bd1b0
Yansen Tjandra <yansentjandra@gmail.com>
2019-04-20 21:46:42 -0400

    Add google-cloud firestore

--- client/app.js
+++ client/app.js
@@ -1,5 +1,24 @@
-
-
 function submit(){
-    
+  var crime = document.getElementById('formCrime').value;
+  var date = document.getElementById('formDate').value;
+  var time = document.getElementById('formTime').value;
+  var location = document.getElementById('formLocation').value;
+  var details = document.getElementById('formDetails').value;
+  alert(crime);
+  $.ajax({
+  type: "POST",
+  url: "http://localhost:3001/input/",
+  dataType: "json",
+  data: {
+    crime: crime,
+    date: date,
+    time: time,
+    location: location,
+    details: details,
+  },
+  success: function(result){
+    console.log("Success");
+    console.log(crime);
+  }
+});
 }
